3b6760b25fc6ca8321a101be99ac167d84cff4a4
Lin <lincarbajales@gmail.com>
2025-06-02 12:35:34 +0200

    Incluida funcionalidad JavaScript

--- script.js
+++ script.js
@@ -12,3 +12,12 @@ function changeBackground() {
 }
 
 btn.addEventListener("click", changeBackground);
+
+document.addEventListener("DOMContentLoaded", function() {
+    const boton = document.querySelector(".button-toggle");
+    const menuLinks = document.querySelector(".menu");
+
+    boton.addEventListener("click", function(){
+        menuLinks.classList.toggle("open");
+    })
+})
