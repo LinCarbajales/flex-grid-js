f7cb048449b5b4acd1327a2be24311db808d8243
Lin <lincarbajales@gmail.com>
2025-06-02 11:56:36 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hola, mundo");
+
+const btn = document.querySelector("#cambia-color"); // Selecciona por id
+
+function random(number) {
+  return Math.floor(Math.random() * (number + 1));
+}
+
+function changeBackground() {
+  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+  document.body.style.backgroundColor = rndCol;
+}
+
+btn.addEventListener("click", changeBackground);
